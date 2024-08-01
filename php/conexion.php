b6c6c4eeca3df67a0353e84f485d5ca438e12e7f
agosnova <114870849+agosnova@users.noreply.github.com>
2024-08-01 17:42:27 -0300

    Delete php directory

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "credenciales.php";
-
-$conexion=new mysqli(HOST,USER,PASS,DB);
-	if($conexion-> connect_error){
-		die("no se puedo conectar al servidor");
-	}
-
- ?>
--- php/credenciales.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define("HOST","mattprofe.com.ar");
-define("USER","app-ades");
-define("PASS","appades1234");
-define("DB","app-ades");
- ?>
